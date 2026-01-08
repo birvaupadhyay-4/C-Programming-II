f49bf25dbbef6455973f19623d2619fae8970802
Birva Upadhyay-25BEE020 <birvaupadhyay2007@gmail.com>
2026-01-08 22:07:09 +0530

    Add files via upload

---
+++ area perimeter of square.py
@@ -0,0 +1,4 @@
+a=int(input("Length of square:"))
+b=a*a
+c=4*a
+print(f"Area={b} amd perimeter={c}")
---
+++ swap 2 values.py
@@ -0,0 +1,6 @@
+a=int(input("Enter value of a:"))
+b=int(input("Enter value of b:"))
+temp=a
+a=b
+b=temp
+print(f"New value of a={a} and b={b}")
