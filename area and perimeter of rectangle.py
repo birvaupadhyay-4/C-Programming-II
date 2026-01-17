e5e59fcfd42ef3d456b16d64a6b2c6929129af99
Birva Upadhyay-25BEE020 <birvaupadhyay2007@gmail.com>
2026-01-17 16:28:32 +0530

    Add files via upload

---
+++ area and perimeter of rectangle.py
@@ -0,0 +1,10 @@
+l=int(input("Enter length:"))
+b=int(input("Enter breadth:"))
+
+p=2*(l+b)
+a=l*b
+
+if(p>a):
+    print("perimeter of rectangle is greater than area")
+else:
+    print("area of tectangle is greater than the perimeter")
