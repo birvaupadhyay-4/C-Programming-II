2fa09f6b260e68a3d91d64022af6549f465d998f
Birva Upadhyay-25BEE020 <birvaupadhyay2007@gmail.com>
2026-01-26 22:49:06 +0530

    Delete total and avg of 3 subjects.py

--- total and avg of 3 subjects.py
+++
@@ -1,5 +0,0 @@
-a=int(input("Enter marks of 1st subject:"))
-b=int(input("Enter marks of 2nd subject:"))
-c=int(input("Enter marks of 3rd subject:"))
-print(f"Total={a+b+c},Average of three subjects={(a+b+c)/3}")
-
