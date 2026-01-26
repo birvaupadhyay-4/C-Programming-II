c8061cef2a0b97743e5a08b4573e34c229f63cb7
Birva Upadhyay-25BEE020 <birvaupadhyay2007@gmail.com>
2026-01-26 22:47:08 +0530

    Delete triangle valid or not.py

--- triangle valid or not.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter angle BAC:"))
-b=int(input("Enter angle ABC:"))
-c=int(input("Enter angle BCA:"))
-
-if(a+b+c==180):
-    print("The triangle is valid")
-else:
-    print("The triangle is invalid")
-
