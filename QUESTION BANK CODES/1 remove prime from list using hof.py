f47fb01bf85884511d1c0b03aa47c213aafee26e
Birva Upadhyay-25BEE020 <birvaupadhyay2007@gmail.com>
2026-04-22 20:25:32 +0530

    Fix prime function logic and retain list removal

--- QUESTION BANK CODES/1 remove prime from list using hof.py
+++ QUESTION BANK CODES/1 remove prime from list using hof.py
@@ -2,7 +2,9 @@ lst=list(map(int,input("Enter list:").split()))
 def prime(n):
     if n<2:
         return False
-    elif n>2:
+    elif n==2:
+        return True
+    else:    
         for i in range(2,n):
             if n%i==0:
                 return False
